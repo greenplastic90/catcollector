bfd34cf285e7c61c1709810fe1d033a86503eabb
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-07-27 15:39:05 +0300

    created path and details view for showing a cat

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-# Add this cats list below the imports
-cats = [
-    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-    {'name': 'Sachi', 'breed': 'calico',
-        'description': 'gentle and loving', 'age': 2},
-]
+from .models import Cat
+
+
 # Create your views here.
 
 # Define the home view
@@ -22,7 +19,13 @@ def about(request):
 
 
 def cats_index(request):
+    cats = Cat.objects.all()
     # We pass data to a template very much like we did in Express!
     return render(request, 'cats/index.html', {
         'cats': cats
     })
+
+
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
