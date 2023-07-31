a5d2fcf9414210ba20369c64187affc1c14bddc9
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-07-31 13:33:55 +0300

    updated cats details template to show feedings

--- main_app/models.py
+++ main_app/models.py
@@ -23,9 +23,9 @@ class Cat(models.Model):
 
 
 class Feeding(models.Model):
-    date = models.DateField()
+    date = models.DateField('feeding date')
     meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.get_meal_display()} on {self.date}"
+        return f"{self.get_meal_display()} on {self.date} - {self.cat}"
